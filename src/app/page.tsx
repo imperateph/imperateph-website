dd54cdd7ca63056fa433d7f9906f2261d52d60f2
kimzii <kimzietorres@gmail.com>
2024-09-24 22:50:45 +0800

    changed bg to transparent and changed font to white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,27 +46,25 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="xl:bg-[url('/bgimage.png')] bg-cover xl:h-full flex items-center">
+    <main className="xl:bg-[url('/bgimage.png')] bg-cover xl:h-full flex items-center text-white">
       <BoxMaxWidth className="mr-0">
         <Image
           src={image}
           alt="Showcase"
           className="max-h-[500px] min-h-[450px] w-full object-cover xl:hidden"
         />
-        <div className="mx-auto flex flex-col xl:flex-row bg-white rounded-lg p-6 xl:mb-auto xl:mr-8 gap-6">
+        <div className="mx-auto flex flex-col xl:flex-row bg-transparent rounded-lg p-6 xl:mb-auto xl:mr-8 gap-6">
           <Box className="text-center xl:text-left xl:w-[400px]">
+            <p>
+              Discover Exceptional Real Estate Tailored to Your Lifestyle and
+              Goals
+            </p>
             <div className="mb-4">
               <h1 className="text-6xl font-extrabold">Your Future Matters,</h1>
               <h1 className="text-6xl font-extrabold">
-                Invest with <span className=" text-blue-800">Imperate</span>
+                Invest with <span>Imperate</span>
               </h1>
             </div>
-            <p>
-              We showcase the finest upscale and luxury properties across the
-              Philippines. Our curated selection offers a gateway to discover
-              exceptional living spaces tailored to your lifestyle and
-              investment goals.
-            </p>
           </Box>
           <Box className="mx-auto max-w-[500px]">
             <p className="mb-4 font-bold text-xl">
