eb50c801d9bad2e542e565c85cb250208cb8811f
John Roice Aldeza <jroicealdeza@gmail.com>
2024-09-25 13:42:55 +0800

    Refactor font styles to use Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export const metadata: Metadata = {
     "davao",
     "luxury homes",
     "luxury condos",
-
   ],
   description:
     "We showcase the finest upscale and luxury properties across the Philippines. Our curated selection offers a gateway to discover exceptional living spaces tailored to your lifestyle and investment goals.",
@@ -31,13 +30,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
-    <html lang="en" className={`${font.poppins} antialiased`} style={{height: "100%"}}>
-      <body style={{height: "100%"}}>
+    <html lang="en" className={`${font.poppins.variable}`}>
+      <body>
         <Providers>
-          <div className="flex flex-col min-h-screen" style={{height: "100%"}}>
+          <div className="flex flex-col min-h-screen">
             <Navbar />
-            {children}
+            <div>{children}</div>
           </div>
         </Providers>
       </body>
--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -4,6 +4,7 @@ import { Poppins } from "next/font/google";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: "400",
+  variable: "--font-poppins"
 });
 
 export const font = {
